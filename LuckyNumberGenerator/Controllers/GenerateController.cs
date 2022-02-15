72e8af2ee91cfe53457fdb9b108f8240f3c8ceeb
Niels Filter <filterniels@gmail.com>
2022-02-15 22:28:34 +0200

    higher number

--- LuckyNumberGenerator/Controllers/GenerateController.cs
+++ LuckyNumberGenerator/Controllers/GenerateController.cs
@@ -16,7 +16,7 @@ namespace LuckyNumberGenerator.Controllers
         [HttpGet("number", Name = "GenerateNumber")]
         public int Get()
         {
-            var number = new Random().Next(0, 100);
+            var number = new Random().Next(1000, 9000);
             _logger.LogInformation("Generated number {0}", number);
             return number;
         }
