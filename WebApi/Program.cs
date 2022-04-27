171a44e9c0e0ffa8b22ae3485e5f91ef011cbfe7
Niels <filterniels@gmail.com>
2022-04-27 23:33:54 +0200

    Added application insights

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -21,6 +21,8 @@ builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry();
+// builder.Services.AddApplicationInsightsKubernetesEnricher();
 
 var app = builder.Build();
 
