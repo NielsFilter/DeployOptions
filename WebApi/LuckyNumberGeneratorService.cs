82630017ff89d80582c4bf0e5c445fc1b1880cbd
Niels <filterniels@gmail.com>
2022-06-03 22:38:54 +0200

    logging http address

--- WebApi/LuckyNumberGeneratorService.cs
+++ WebApi/LuckyNumberGeneratorService.cs
@@ -3,14 +3,17 @@ namespace WebApi
     public class LuckyNumberGeneratorService
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger _logger;
 
-        public LuckyNumberGeneratorService(HttpClient httpClient)
+        public LuckyNumberGeneratorService(HttpClient httpClient, ILogger logger)
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task<int> FetchLuckyNumberOfTheDay()
         {
+            _logger.LogInformation("Http client address is '{0}'", _httpClient.BaseAddress);
             return await _httpClient.GetFromJsonAsync<int>("generate/number");
         }
     }
