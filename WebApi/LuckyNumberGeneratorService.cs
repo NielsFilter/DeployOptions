aa378b55ca4cee1928c5756274d97766115b2708
Niels Filter <filterniels@gmail.com>
2022-06-07 20:21:15 +0200

    Logger DI wasn't correct for lucky num gen

--- WebApi/LuckyNumberGeneratorService.cs
+++ WebApi/LuckyNumberGeneratorService.cs
@@ -3,17 +3,14 @@ namespace WebApi
     public class LuckyNumberGeneratorService
     {
         private readonly HttpClient _httpClient;
-        private readonly ILogger _logger;
 
-        public LuckyNumberGeneratorService(HttpClient httpClient, ILogger logger)
+        public LuckyNumberGeneratorService(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _logger = logger;
         }
 
         public async Task<int> FetchLuckyNumberOfTheDay()
         {
-            _logger.LogInformation("Http client address is '{0}'", _httpClient.BaseAddress);
             return await _httpClient.GetFromJsonAsync<int>("generate/number");
         }
     }
