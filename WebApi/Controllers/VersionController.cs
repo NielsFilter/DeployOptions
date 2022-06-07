76fc0a096f15b398b4cc19706bc15ea56ed03367
niels.filter <niels@kohde.io>
2022-06-07 22:17:21 +0200

    Add version endpoints

---
+++ WebApi/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        public VersionController()
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
